c80bd6d5595dae01cb63fbf92c3accd80f1d4e50
manjumh021 <81350837+manjumh021@users.noreply.github.com>
2022-11-24 12:37:20 +0000

    github-actions/create-ec2-instance: added github action and terraform files.

---
+++ terraform/create-ec2/main.tf
@@ -0,0 +1,63 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region  = "us-west-1"
+}
+
+resource "aws_instance" "snipe-it_server" {
+  ami = "ami-02ea247e531eb3ce6"
+  instance_type = "t2.micro"
+  key_name = "<private key file name>"
+  vpc_security_group_ids = [aws_security_group.main.id]
+  tags = {
+    Name = "dev-environment"
+  }
+}
+
+resource "aws_security_group" "main" {
+  egress = [
+    {
+      cidr_blocks      = ["0.0.0.0/0", ]
+      description      = ""
+      from_port        = 0
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "-1"
+      security_groups  = []
+      self             = false
+      to_port          = 0
+    }
+  ]
+  ingress = [
+    {
+      cidr_blocks      = ["0.0.0.0/0", ]
+      description      = ""
+      from_port        = 22
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "tcp"
+      security_groups  = []
+      self             = false
+      to_port          = 22
+    },
+    {
+      cidr_blocks      = ["0.0.0.0/0", ]
+      description      = ""
+      from_port        = 8000
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "tcp"
+      security_groups  = []
+      self             = false
+      to_port          = 8000
+    }
+  ]
+}
